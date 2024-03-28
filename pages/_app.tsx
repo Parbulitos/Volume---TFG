48ee2fe7fd78a62e0e3f4491c8d2e6347ca0645f
Hugo Calzado <hcalzado@gmail.com>
2024-03-28 12:44:49 +0100

    Componentes del layout creados

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,11 @@
 import type { AppProps } from "next/app";
 
 import "../app/globals.css";
-import Footer from "./layout/footer";
-import Navbar from "./layout/navbar";
+import { Footer, Navbar } from './layout/exports'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen bg-gray-900">
       <Navbar />
       <div className="flex-grow">
         <Component {...pageProps} />
