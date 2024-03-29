c80515899d0979ef5a4b384a81f928bf4d9579c0
HugoCalzado <53238187+Orhugo@users.noreply.github.com>
2024-03-29 17:06:14 +0100

    Vltfg 33 - Página principal (#5)
    * commit test (#2)
    
    * Cambiado el gitignore para que excluya el env
    
    * Comienzo de la página principal
    
    * dummy commit
    
    * Solucionado error de buildeo en rama 33
    
    * Eliminado dummy commit
    
    * Ajuste tailwind en layout y conf. rama
    
    ---------
    
    Co-authored-by: Carlos Echavarne <107615048+Parbulitos@users.noreply.github.com>
    Co-authored-by: Parbulitos <cechavarne@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,13 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+
+import "../app/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+  return (
+    <div className="flex flex-col min-h-screen bg-gray-900">
+      <div className="flex-grow">
+        <Component {...pageProps} />
+      </div>
+    </div>
+  );
 }
