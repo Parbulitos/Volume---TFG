221c4522c86c1841aa45ab41b74c95564cab033c
Hugo Calzado <hcalzado@gmail.com>
2024-03-29 17:03:56 +0100

    Ajuste tailwind en layout y conf. rama

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,13 @@
 import type { AppProps } from "next/app";
 
 import "../app/globals.css";
-import Footer from "@/layout/footer";
-import Navbar from "@/layout/navbar";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className="flex flex-col min-h-screen bg-gray-900">
-      <Navbar />
       <div className="flex-grow">
         <Component {...pageProps} />
       </div>
-      <Footer />
     </div>
   );
 }
