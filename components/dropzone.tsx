465ac860d38e62479c24603b67afe9e7808400f5
Hugo Calzado <hcalzado@gmail.com>
2024-04-10 13:03:01 +0200

    VLTFG-42 Continuación upload file

--- components/dropzone.tsx
+++ components/dropzone.tsx
@@ -1,49 +1,75 @@
 import { useCallback } from 'react';
 import { useDropzone } from 'react-dropzone';
 
-const Dropzone = ({ multipleFiles, onModelsDrop }: { multipleFiles: boolean, onModelsDrop: Function }) => {
+const Dropzone = ({
+    multipleFiles,
+    onModelsDrop,
+    size,
+}: {
+    multipleFiles: boolean;
+    onModelsDrop: Function;
+    size: { width: number; height: number };
+}) => {
+    const onDrop = useCallback(
+        (acceptedFiles: any, fileRejections: string | any[]) => {
+            // Procesa los archivos aceptados
+            onModelsDrop(acceptedFiles);
+            console.log('Archivos aceptados: ', acceptedFiles);
 
-  const onDrop = useCallback((acceptedFiles: any, fileRejections: string | any[]) => {
-    // Procesa los archivos aceptados
-    onModelsDrop(acceptedFiles);
-    console.log("Archivos aceptados: ", acceptedFiles);
-    
-    // Muestra una alerta si hay archivos rechazados
-    if (fileRejections.length > 0) {
-      alert("Solo se pueden subir archivos en formato .stl");
-    }
-  }, [onModelsDrop]);
+            // Muestra una alerta si hay archivos rechazados
+            if (fileRejections.length > 0) {
+                alert('Solo se pueden subir archivos en formato .stl');
+            }
+        },
+        [onModelsDrop]
+    );
 
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({
-    onDrop,
-    accept: { "model/stl": [".stl"] }, // Solo acepta archivos .stl
-    multiple: multipleFiles
-  });
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({
+        onDrop,
+        accept: { 'model/stl': ['.stl'] }, // Solo acepta archivos .stl
+        multiple: multipleFiles,
+    });
 
-  const dragActiveMessage: string = !multipleFiles ? "Suelta tu archivo aquí..." : "Suelta tus archivos aquí..."
-  const dragInactiveMessage: string = !multipleFiles ? "Arrastra y suelta tu archivo o haz click para importarlo..." : "Arrastra y suelta tus archivos o haz click para importarlos..."
+    const dragActiveMessage: string = !multipleFiles
+        ? 'Suelta tu archivo aquí...'
+        : 'Suelta tus archivos aquí...';
+    const dragInactiveMessage: string = !multipleFiles
+        ? 'Arrastra y suelta tu archivo o haz click para importarlo...'
+        : 'Arrastra y suelta tus archivos o haz click para importarlos...';
 
-  return (
-    <div
-      {...getRootProps({
-        className: `flex w-[700px] h-[100px] justify-center items-center border-2 border-dashed ${
-          isDragActive ? "border-violet-400 bg-violet-700" : "border-gray-300"
-        }`
-      })}
-    >
-      <input {...getInputProps()} />
-      {isDragActive ? (
-        <p className='text-xl font-bold text-white'>{dragActiveMessage}</p>
-      ) : (
-        <div className="flex flex-col items-center">
-          <p className="text-xl font-bold">
-            {dragInactiveMessage}
-          </p>
-          <p className="text-lg">Solo archivos en formato STL</p>
+    // Establecer estilos dinámicamente usando el atributo style
+    const customStyle = {
+        width: `${size.width}px`, // Uso de template literals para asignar el ancho
+        height: `${size.height}px`, // y el alto dinámicamente
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        border: '2px dashed',
+        borderColor: isDragActive ? '#9F7AEA' : 'white',
+        backgroundColor: isDragActive ? '#6B46C1' : 'transparent',
+        color: 'white',
+    };
+
+    return (
+        <div
+            {...getRootProps({
+                style: customStyle, // Establecer los estilos dinámicamente
+                //   className: `flex w-[${size.width}px] h-[${size.height}px] justify-center items-center text-white border-2 border-dashed ${
+                //     isDragActive ? "border-violet-400 bg-violet-700" : "border-gray-300"
+                //   }`
+            })}
+        >
+            <input {...getInputProps()} />
+            {isDragActive ? (
+                <p className='text-xl font-bold text-white'>{dragActiveMessage}</p>
+            ) : (
+                <div className='flex flex-col items-center'>
+                    <p className='text-xl font-bold'>{dragInactiveMessage}</p>
+                    <p className='text-lg'>Solo archivos en formato STL</p>
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 };
 
 export default Dropzone;
--- pages/upload-file/index.tsx
+++ pages/upload-file/index.tsx
@@ -1,13 +1,41 @@
-import Dropzone from '@/components/dropzone'
-import React from 'react'
-
+import Dropzone from '@/components/dropzone';
+import React from 'react';
 
 const UploadFile = () => {
-  return (
-    <div>
-        <Dropzone multipleFiles={true} onModelsDrop={() => {}}/>
-    </div>
-  )
-}
+    return (
+        <div className='flex flex-col items-center mt-5 w-full'>
+            <Dropzone
+                multipleFiles={true}
+                onModelsDrop={() => {}}
+                size={{ width: 1000, height: 200 }}
+            />
 
-export default UploadFile
+            <h2 className='text-white text-3xl font-bold mt-6 font'>Información Requerida</h2>
+            <div className='grid grid-cols-2 items-center w-[1000px] mt-3 gap-x-16 gap-y-5'>
+                <input
+                    type='text'
+                    placeholder='Nombre del modelo'
+                    className='input input-bordered'
+                />
+                <select className='select select-bordered'>
+                    <option disabled selected>
+                        Categoría
+                    </option>
+                    <option>Cat 1</option>
+                    <option>Cat 2</option>
+                    <option>Cat 3</option>
+                    <option>Cat 4</option>
+                    <option>Cat 5</option>
+                </select>
+                <input
+                    type='text'
+                    placeholder='Tags'
+                    className='input input-bordered'
+                />
+                <input type="checkbox" defaultChecked className="checkbox checkbox-primary" />
+            </div>
+        </div>
+    );
+};
+
+export default UploadFile;
