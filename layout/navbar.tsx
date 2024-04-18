d733924e5a0d84991af25ec5c2499edf00a24729
Hugo Calzado <hcalzado@gmail.com>
2024-04-18 18:39:47 +0200

    VLTFG-18 - Ajustes responsive

--- layout/navbar.tsx
+++ layout/navbar.tsx
@@ -1,52 +1,114 @@
 import React from 'react';
 import { useState } from 'react';
-import logo from '../public/volumeIcon.ico';
 import Link from 'next/link';
 import Image from 'next/image';
 
+import logo from '../public/Logo.png';
+import avatar from '../public/avatar.svg';
+
 const Navbar = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  return (
-    <header className="flex justify-between items-center py-4 px-10 bg-gray-900 text-white">
-      <Link href="/" >
-        <Image src={logo} alt="logo" width={40} height={40} />
-      </Link>
-      <nav className="hidden md:flex md:items-center">
-        <ul className="flex">
-          <li className="px-10">
-            <Link href="#" className="transition duration-300 ease-in-out hover:text-violet-400 text-2xl">Catálogo</Link>
-          </li>
-          <li className="px-10">
-            <Link href="#" className="transition duration-300 ease-in-out hover:text-violet-400 text-2xl">Presupuesto</Link>
-          </li>
-          <li className="px-10">
-            <Link href="#" className="transition duration-300 ease-in-out hover:text-violet-400 text-2xl">Tutoriales</Link>
-          </li>
-          <li className="px-10">
-            <Link href="#" className="transition duration-300 ease-in-out hover:text-violet-400 text-2xl">Subir Archivo</Link>
-          </li>
-        </ul>
-      </nav>
-      <Link href="/sign" className="hidden md:block px-6 py-2 bg-violet-500 rounded-full cursor-pointer transition duration-300 ease-in-out hover:bg-violet-700">Cuenta</Link>
-      <p className="md:hidden cursor-pointer" onClick={() => setIsMenuOpen(!isMenuOpen)}>Menu</p>
+    const username = 'testUser';
 
-      {/* Overlay para móvil */}
-      {isMenuOpen && (
-        <div className="fixed inset-0 bg-gray-900 z-10">
-          <div className="absolute top-5 right-10 text-4xl" onClick={() => setIsMenuOpen(false)}>
-            &times;
-          </div>
-          <div className="flex flex-col items-center justify-center h-full space-y-6">
-            <a href="#" className="text-xl transition duration-300 ease-in-out hover:text-violet-400">Catálogo</a>
-            <a href="#" className="text-xl transition duration-300 ease-in-out hover:text-violet-400">Presupuesto</a>
-            <a href="#" className="text-xl transition duration-300 ease-in-out hover:text-violet-400">Tutoriales</a>
-            <a href="#" className="text-xl transition duration-300 ease-in-out hover:text-violet-400">Subir Archivo</a>
-          </div>
-        </div>
-      )}
-    </header>
-  );
+    return (
+        <header className='flex justify-between items-center py-4 px-10 bg-gray-900 text-white'>
+            <Link href='/'>
+                <Image src={logo} alt='logo' width={40} height={40} />
+            </Link>
+            <nav className='hidden md:flex md:items-center'>
+                <ul className='flex'>
+                    <li className='px-5 lg:px-10'>
+                        <Link
+                            href='/catalog'
+                            className='transition duration-300 ease-in-out hover:text-violet-400 lg:text-2xl'
+                        >
+                            Catálogo
+                        </Link>
+                    </li>
+                    <li className='px-5 lg:px-10'>
+                        <Link
+                            href='/budget'
+                            className='transition duration-300 ease-in-out hover:text-violet-400 lg:text-2xl'
+                        >
+                            Presupuesto
+                        </Link>
+                    </li>
+                    <li className='px-5 lg:px-10'>
+                        <a
+                            href='#'
+                            className='transition duration-300 ease-in-out hover:text-violet-400 lg:text-2xl'
+                        >
+                            Tutoriales
+                        </a>
+                    </li>
+                    <li className='px-5 lg:px-10'>
+                        <Link
+                            href='/upload-file'
+                            className='transition duration-300 ease-in-out hover:text-violet-400 lg:text-2xl'
+                        >
+                            Subir Archivo
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+            <Link href={`/user-profile/${encodeURIComponent(username)}`} className='hidden md:block'>
+                <div className='avatar'>
+                    <div className='w-12 rounded-full bg-white'>
+                        <Image src={avatar} alt='Avatar' />
+                    </div>
+                </div>
+            </Link>
+            <p className='md:hidden cursor-pointer' onClick={() => setIsMenuOpen(!isMenuOpen)}>
+                Menu
+            </p>
+
+            {/* Overlay para móvil */}
+            {isMenuOpen && (
+                <div className='fixed inset-0 bg-gray-900 z-10'>
+                    <div
+                        className='absolute top-5 right-10 text-4xl'
+                        onClick={() => setIsMenuOpen(false)}
+                    >
+                        &times;
+                    </div>
+                    <div className='flex flex-col items-center justify-center h-full space-y-6'>
+                        <Link
+                            href={`/user-profile/${encodeURIComponent(username)}`}
+                            className='text-xl transition duration-300 ease-in-out hover:text-violet-400'
+                            onClick={() => setIsMenuOpen(false)}
+                        >
+                            Cuenta
+                        </Link>
+                        <Link
+                            href='/catalog'
+                            className='text-xl transition duration-300 ease-in-out hover:text-violet-400'
+                        >
+                            Catálogo
+                        </Link>
+                        <Link
+                            href='/budget'
+                            className='text-xl transition duration-300 ease-in-out hover:text-violet-400'
+                        >
+                            Presupuesto
+                        </Link>
+                        <Link
+                            href='#'
+                            className='text-xl transition duration-300 ease-in-out hover:text-violet-400'
+                        >
+                            Tutoriales
+                        </Link>
+                        <Link
+                            href='/upload-file'
+                            className='text-xl transition duration-300 ease-in-out hover:text-violet-400'
+                        >
+                            Subir Archivo
+                        </Link>
+                    </div>
+                </div>
+            )}
+        </header>
+    );
 };
 
 export default Navbar;
--- pages/sign/index.tsx
+++ pages/sign/index.tsx
@@ -11,24 +11,45 @@ const Forms = () => {
     };
 
     return (
-        <div className='flex items-center justify-center w-screen min-h-[700px]'>
-            <div className='relative'>
-                <div
-                    className={`absolute flex flex-col items-center justify-center bg-gradient-to-r bg-secondary to-white rounded-3xl w-[450px] h-[600px] transition-transform duration-500 ${
-                        isPanelRight ? 'translate-x-[450px]' : 'translate-x-0'
-                    }`}
-                >
-                    <p className='text-2xl font-bold text-center'>{isPanelRight ? <>¡Hey!<br/>Registrate para acceder a todo el contenido</> : '¿Ya tienes cuenta?'}</p>
-                    <button className='btn btn-lg btn-secondary border-white shadow-2xl mt-8' onClick={togglePanel} >
-                        {isPanelRight ? 'Regístrate' : 'Inicia Sesión'}
-                    </button>
-                </div>
-                <div className='flex justify-between bg-white rounded-3xl w-[900px] h-[600px]'>
-                    <SignIn />
-                    <SignUp />
+        <>
+            <div className='hidden md:flex items-center justify-center w-screen min-h-[700px]'>
+                <div className='relative'>
+                    <div
+                        className={`hidden md:flex absolute flex-col items-center justify-center bg-gradient-to-r bg-secondary to-white rounded-3xl md:w-[350px] lg:w-[450px] h-[600px] transition-transform duration-500 ${
+                            isPanelRight
+                                ? 'md:translate-x-[350px] lg:translate-x-[450px]'
+                                : 'translate-x-0'
+                        }`}
+                    >
+                        <p className='md:text-xl lg:text-2xl font-bold text-center'>
+                            {isPanelRight ? (
+                                <>
+                                    ¡Hey!
+                                    <br />
+                                    Registrate para acceder a todo el contenido
+                                </>
+                            ) : (
+                                '¿Ya tienes cuenta?'
+                            )}
+                        </p>
+                        <button
+                            className='btn btn-lg btn-secondary border-white shadow-2xl mt-8'
+                            onClick={togglePanel}
+                        >
+                            {isPanelRight ? 'Regístrate' : 'Inicia Sesión'}
+                        </button>
+                    </div>
+                    <div className='hidden md:flex justify-between bg-white rounded-3xl md:w-[700px] lg:w-[900px] h-[600px]'>
+                        <SignIn />
+                        <SignUp />
+                    </div>
                 </div>
             </div>
-        </div>
+            <div className='md:hidden bg-white rounded-3xl flex mx-6 mb-6 justify-center cursor-pointer'>
+                {isPanelRight ? (<SignIn onFormChange={togglePanel}/>) : (<SignUp onFormChange={togglePanel}/>)}
+                
+            </div>
+        </>
     );
 };
 
