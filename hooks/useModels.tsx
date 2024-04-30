ed5c6d30596e770eaabeb1b2c3a393aa1187a93f
Parbulitos <cechavarne@gmail.com>
2024-04-30 19:05:02 +0200

    models cloud

--- hooks/useModels.tsx
+++ hooks/useModels.tsx
@@ -1,4 +1,5 @@
-import {Models, Users} from "@prisma/client";
+import { createClient } from '@/database/utils';
+import { Models } from '@prisma/client';
 
 export const useModels = ()=>{
     const getModelById = async (id: string): Promise<Models> => {
@@ -9,13 +10,20 @@ export const useModels = ()=>{
         return (await fetch(`/api/models/getallmodels`)).json()
     }
 
-    const addModel = async (model: Models) => {
+    const addModel = async (model: Models, userId: string, file: File) => {
+        const supabase = createClient()
+        const { data, error } = await supabase.storage.from('ModelsBucket').upload(`${userId}/${file.name}`, file)
+        if (error) {
+            console.error('An error has occured no model was uploaded: ', error)
+            throw error
+        }
+        const newModel: Models = {...model, fileUrl: data?.path}
         return (await fetch(`/api/models/addmodel`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify(model)
+            body: JSON.stringify(newModel)
         }))
     }
 
