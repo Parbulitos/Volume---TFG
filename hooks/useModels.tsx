120154cc0cf97e6f84f52c8f24a3f68059e20142
Hugo Calzado <hcalzado@gmail.com>
2024-05-06 17:24:46 +0200

    Cambio del formulario de subida

--- hooks/useModels.tsx
+++ hooks/useModels.tsx
@@ -1,16 +1,16 @@
-import { createClient } from '@/database/utils';
+import { supabaseClient } from '@/database/utils';
 import { Models } from '@prisma/client';
 
-export const useModels = ()=>{
-    const supabase = createClient()
-
+export const useModels = () => {
     const getModelById = async (id: string): Promise<Models> => {
-        return (await fetch(`/api/models/getmodelbyid?id=${id}`)).json().then((res) => res.model)
-    }
+        return (await fetch(`/api/models/getmodelbyid?id=${id}`)).json().then((res) => res.model);
+    };
 
-    const getModelFileById = (userId: string, fileName: string)=>{
-        const dirName = fileName.split('.').reverse().pop()
-        const { data } = supabase.storage.from('ModelsBucket').getPublicUrl(`${userId}/${dirName}/${fileName}`)
+    const getModelFileById = (userId: string, fileName: string) => {
+        const dirName = fileName.split('.').reverse().pop();
+        const { data } = supabaseClient.storage
+            .from('ModelsBucket')
+            .getPublicUrl(`${userId}/${dirName}/${fileName}`);
         // Verifica si se obtuvo la URL correctamente
         if (data && data.publicUrl) {
             // Crea un enlace temporal en el DOM
@@ -23,43 +23,49 @@ export const useModels = ()=>{
             // Haz clic en el enlace para iniciar la descarga
             link.click();
         }
-    }
+    };
 
-    const getAllModels = async(): Promise<Models[]> =>{
-        return (await fetch(`/api/models/getallmodels`)).json()
-    }
+    const getAllModels = async (): Promise<Models[]> => {
+        return (await fetch(`/api/models/getallmodels`)).json();
+    };
 
-    const addModel = async (model: Omit<Models, "id">, userId: string, file: File) => {
-        const dirName = file.name.split('.').reverse().pop()
-        const { data, error } = await supabase.storage.from('ModelsBucket').upload(`${userId}/${dirName}/${file.name}`, file)
+    const addModel = async (model: Omit<Models, 'id'>, userId: string, file: File) => {
+        const dirName = file.name.split('.').reverse().pop();
+        const { data, error } = await supabaseClient.storage
+            .from('ModelsBucket')
+            .upload(`${userId}/${dirName}/${file.name}`, file);
         if (error) {
-            console.error('An error has occured no model was uploaded: ', error)
-            throw error
+            console.error('An error has occured no model was uploaded: ', error);
+            throw error;
         }
-        const newModel = {...model, fileUrl: data?.path}
-        console.log(data?.path)
-        return (await fetch(`/api/models/addmodel`, {
+        const newModel = { ...model, fileUrl: data?.path };
+        console.log(data?.path);
+        return await fetch(`/api/models/addmodel`, {
             method: 'POST',
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
             },
-            body: JSON.stringify(newModel)
-        }))
-    }
+            body: JSON.stringify(newModel),
+        });
+    };
 
-    const deleteModelById = async (id: string)=>{
-        return (await fetch(`/api/models/deletemodelbyid?id=${id}`,{
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        })).json().then()
-    }
+    const deleteModelById = async (id: string) => {
+        return (
+            await fetch(`/api/models/deletemodelbyid?id=${id}`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            })
+        )
+            .json()
+            .then();
+    };
 
     return {
         getModelById,
         getAllModels,
         addModel,
-        getModelFileById
-    }
-}
+        getModelFileById,
+    };
+};
--- pages/upload-file/index.tsx
+++ pages/upload-file/index.tsx
@@ -1,17 +1,64 @@
 import Dropzone from '@/components/dropzone';
+import { useModels } from '@/hooks/useModels';
+import { useUserContext } from '@/hooks/useUserContext';
+import { Models } from '@prisma/client';
 import Image from 'next/image';
 import React, { ChangeEvent, useRef, useState } from 'react';
+
+interface FormState {
+    modelName: string;
+    category: string;
+    tags: string;
+    images: File[];
+    isMonetizable: boolean;
+    isRemix: boolean;
+    publishAnonymously: boolean;
+    description: string;
+}
 
 const UploadFile = () => {
     const [droppedModels, setDroppedModels] = useState<File[]>([]);
     const [isOpen, setIsOpen] = useState<boolean>(true);
+    const { addModel, getModelFileById } = useModels();
     const [images, setImages] = useState<File[]>([]);
     const imageInputRef = useRef(null);
+    const [formState, setFormState] = useState<FormState>({
+        modelName: '',
+        category: '',
+        tags: '',
+        images: [],
+        isMonetizable: false,
+        isRemix: false,
+        publishAnonymously: false,
+        description: '',
+    });
+
+    const { userDetails } = useUserContext();
 
     const onModelsDrop = (acceptedFiles: File[]) => {
         if (acceptedFiles.length === 0) return;
         setDroppedModels((prevFiles) => [...prevFiles, ...acceptedFiles]);
         console.log('Archivos aceptados: ', acceptedFiles);
+    };
+
+    const upload = () => {
+        const uploadTime = new Date();
+        const file = droppedModels.pop();
+        const model = {
+            collectionId: null,
+            description: null,
+            fileUrl: '',
+            grade: null,
+            likes: null,
+            name: file?.name,
+            ownerId: userDetails?.id,
+            uploadTime: uploadTime,
+            views: 0,
+        } as Omit<Models, 'id'>;
+        if (!file) return;
+        addModel(model, '123', file);
+        // Download file
+        // getModelFileById('123','testModel.stl')
     };
 
     const handleRemoveModel = (index: number) => {
@@ -20,6 +67,21 @@ const UploadFile = () => {
             newModels.splice(index, 1);
             return newModels;
         });
+    };
+
+    const handleInputChange = (
+        event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>
+    ) => {
+        const { name, value, type, files } = event.target as HTMLInputElement; // Default cast to HTMLInputElement
+
+        if (type === 'file' && files) {
+            setFormState((prev) => ({ ...prev, images: Array.from(files) }));
+        } else if (type === 'checkbox') {
+            const { checked } = event.target as HTMLInputElement; // Safe to access 'checked' here
+            setFormState((prev) => ({ ...prev, [name]: checked }));
+        } else {
+            setFormState((prev) => ({ ...prev, [name]: value }));
+        }
     };
 
     const getImages = (event: ChangeEvent<HTMLInputElement>) => {
@@ -37,10 +99,15 @@ const UploadFile = () => {
         });
     };
 
+    const handleSubmit = (event: React.FormEvent) => {
+        event.preventDefault();
+        // Process your form data here
+        console.log(formState);
+    };
+
     return (
         <div className="mx-auto mb-10 mt-5 flex w-full flex-col items-center justify-center lg:w-2/3">
             <Dropzone multipleFiles={true} onModelsDrop={onModelsDrop} />
-
             {droppedModels.length > 0 && (
                 <div className="collapse collapse-arrow mt-6 w-[90%] bg-base-300 lg:w-[1000px]">
                     <input type="checkbox" checked={isOpen} onChange={() => setIsOpen(!isOpen)} />
@@ -67,85 +134,127 @@ const UploadFile = () => {
             )}
 
             <h2 className="font mt-6 text-3xl font-bold text-white">Información Requerida</h2>
-            <div className="mt-3 flex flex-col items-center gap-x-4 gap-y-5 md:grid md:w-[700px] md:grid-cols-2 lg:w-[1000px] lg:gap-x-16">
-                <input
-                    type="text"
-                    placeholder="Nombre del modelo"
-                    className="input input-bordered w-full bg-white text-black"
-                />
-                <select className="select select-bordered w-full bg-white text-gray-400">
-                    <option defaultValue={''}>Categoría</option>
-                    <option>Cat 1</option>
-                    <option>Cat 2</option>
-                    <option>Cat 3</option>
-                    <option>Cat 4</option>
-                    <option>Cat 5</option>
-                </select>
-                <input
-                    type="text"
-                    placeholder="Tags"
-                    className="input input-bordered w-full bg-white text-black"
-                />
+            <form onSubmit={handleSubmit}>
+                <div className="mt-3 flex flex-col items-center gap-x-4 gap-y-5 md:grid md:w-[700px] md:grid-cols-2 lg:w-[1000px] lg:gap-x-16">
+                    <input
+                        type="text"
+                        name="modelName"
+                        placeholder="Nombre del modelo"
+                        value={formState.modelName}
+                        onChange={handleInputChange}
+                        className="input input-bordered w-full bg-white text-black"
+                    />
+                    <select
+                        className="select select-bordered w-full bg-white text-gray-400"
+                        name="category"
+                        value={formState.category}
+                        onChange={handleInputChange}
+                    >
+                        <option defaultValue={''}>Categoría</option>
+                        <option>Cat 1</option>
+                        <option>Cat 2</option>
+                        <option>Cat 3</option>
+                        <option>Cat 4</option>
+                        <option>Cat 5</option>
+                    </select>
+                    <input
+                        type="text"
+                        name="tags"
+                        placeholder="Tags"
+                        value={formState.tags}
+                        onChange={handleInputChange}
+                        className="input input-bordered w-full bg-white text-black"
+                    />
 
-                <input
-                    type="file"
-                    multiple
-                    accept="image/png, image/jpeg, image/gif"
-                    className="file-input file-input-bordered w-full bg-white text-black"
-                    onChange={getImages}
-                    ref={imageInputRef}
-                />
+                    <input
+                        type="file"
+                        multiple
+                        accept="image/png, image/jpeg, image/gif"
+                        className="file-input file-input-bordered w-full bg-white text-black"
+                        onChange={handleInputChange}
+                        ref={imageInputRef}
+                    />
 
-                {images.length === 1 ? (
-                    <p>{images.length} imágen seleccionada</p>
-                ) : (
-                    <p>{images.length} imágenes seleccionadas</p>
-                )}
-                {images.length > 0 && (
-                    <div className="flex w-full flex-col items-center justify-between py-2 md:col-span-2 md:flex-row">
-                        <div className="relative flex gap-x-4">
-                            {images.map((image, index) => (
-                                <button
-                                    key={index}
-                                    className="relative h-20 w-20 overflow-hidden rounded-lg"
-                                    onClick={() => handleRemoveImage(index)}
-                                >
-                                    <Image
-                                        src={URL.createObjectURL(image)}
-                                        alt={image.name}
-                                        className="h-full w-full object-cover"
-                                        width={80}
-                                        height={80}
-                                    />
-                                    <div className="absolute left-0 top-0 flex h-full w-full items-center justify-center bg-gray-400 bg-opacity-25 opacity-0 hover:opacity-100">
-                                        <span className="text-xl font-bold text-white">×</span>
-                                    </div>
-                                </button>
-                            ))}
+                    {images.length === 1 ? (
+                        <p>{images.length} imágen seleccionada</p>
+                    ) : (
+                        <p>{images.length} imágenes seleccionadas</p>
+                    )}
+                    {images.length > 0 && (
+                        <div className="flex w-full flex-col items-center justify-between py-2 md:col-span-2 md:flex-row">
+                            <div className="relative flex gap-x-4">
+                                {images.map((image, index) => (
+                                    <button
+                                        key={index}
+                                        className="relative h-20 w-20 overflow-hidden rounded-lg"
+                                        onClick={() => handleRemoveImage(index)}
+                                    >
+                                        <Image
+                                            src={URL.createObjectURL(image)}
+                                            alt={image.name}
+                                            className="h-full w-full object-cover"
+                                            width={80}
+                                            height={80}
+                                        />
+                                        <div className="absolute left-0 top-0 flex h-full w-full items-center justify-center bg-gray-400 bg-opacity-25 opacity-0 hover:opacity-100">
+                                            <span className="text-xl font-bold text-white">×</span>
+                                        </div>
+                                    </button>
+                                ))}
+                            </div>
                         </div>
+                    )}
+
+                    <div className="flex w-full flex-col items-center justify-between py-2 md:col-span-2 md:flex-row">
+                        <label className="my-2 flex w-[90%] items-center justify-between gap-x-4 md:w-full md:justify-center lg:flex-row">
+                            <span className="label-text">¿Es monetizable?</span>
+                            <input
+                                type="checkbox"
+                                className="checkbox-primary checkbox"
+                                name="isMonetizable"
+                                checked={formState.isMonetizable}
+                                onChange={handleInputChange}
+                            />
+                        </label>
+                        <label className="my-2 flex w-[90%] items-center justify-between gap-x-4 md:w-full md:justify-center lg:flex-row">
+                            <span className="label-text">¿Es un remix?</span>
+                            <input
+                                type="checkbox"
+                                className="checkbox-primary checkbox"
+                                name="isRemix"
+                                checked={formState.isRemix}
+                                onChange={handleInputChange}
+                            />
+                        </label>
+                        <label className="my-2 flex w-[90%] items-center justify-between gap-x-4 md:w-full md:justify-center lg:flex-row">
+                            <span className="label-text">Publicar como anónimo</span>
+                            <input
+                                type="checkbox"
+                                className="checkbox-primary checkbox"
+                                name="publishAnonymously"
+                                checked={formState.publishAnonymously}
+                                onChange={handleInputChange}
+                            />
+                        </label>
                     </div>
-                )}
 
-                <div className="flex w-full flex-col items-center justify-between py-2 md:col-span-2 md:flex-row">
-                    <label className="my-2 flex w-[90%] items-center justify-between gap-x-4 md:w-full md:justify-center lg:flex-row">
-                        <span className="label-text">¿Es monetizable?</span>
-                        <input type="checkbox" className="checkbox-primary checkbox" />
-                    </label>
-                    <label className="my-2 flex w-[90%] items-center justify-between gap-x-4 md:w-full md:justify-center lg:flex-row">
-                        <span className="label-text">¿Es un remix?</span>
-                        <input type="checkbox" className="checkbox-primary checkbox" />
-                    </label>
-                    <label className="my-2 flex w-[90%] items-center justify-between gap-x-4 md:w-full md:justify-center lg:flex-row">
-                        <span className="label-text">Publicar como anónimo</span>
-                        <input type="checkbox" className="checkbox-primary checkbox" />
-                    </label>
+                    <textarea
+                        className="textarea w-full bg-white text-black md:col-span-2"
+                        placeholder="Descripción del modelo..."
+                        name="description"
+                        value={formState.description}
+                        onChange={handleInputChange}
+                    ></textarea>
                 </div>
-
-                <textarea
-                    className="textarea w-full bg-white text-black md:col-span-2"
-                    placeholder="Descripción del modelo..."
-                ></textarea>
-            </div>
+                <button
+                    type="submit"
+                    onClick={() => {
+                        upload();
+                    }}
+                >
+                    Subir
+                </button>
+            </form>
         </div>
     );
 };
