74ab926673e10c4e7e99a8d3aa807f83a6c1e68c
Carlos Echavarne <107615048+Parbulitos@users.noreply.github.com>
2024-05-06 16:08:58 +0200

    Vltfg 8 (#13)
    * models cloud
    
    * bucketUpload stl
    
    * comment on api data integrity must do

--- hooks/useModels.tsx
+++ hooks/useModels.tsx
@@ -1,21 +1,49 @@
-import {Models, Users} from "@prisma/client";
+import { createClient } from '@/database/utils';
+import { Models } from '@prisma/client';
 
 export const useModels = ()=>{
+    const supabase = createClient()
+
     const getModelById = async (id: string): Promise<Models> => {
         return (await fetch(`/api/models/getmodelbyid?id=${id}`)).json().then((res) => res.model)
+    }
+
+    const getModelFileById = (userId: string, fileName: string)=>{
+        const dirName = fileName.split('.').reverse().pop()
+        const { data } = supabase.storage.from('ModelsBucket').getPublicUrl(`${userId}/${dirName}/${fileName}`)
+        // Verifica si se obtuvo la URL correctamente
+        if (data && data.publicUrl) {
+            // Crea un enlace temporal en el DOM
+            const link = document.createElement('a');
+            link.href = data.publicUrl;
+
+            // Establece el atributo 'download' para indicar al navegador que descargue el archivo
+            link.download = 'download'; // Puedes establecer el nombre de archivo deseado aquí
+
+            // Haz clic en el enlace para iniciar la descarga
+            link.click();
+        }
     }
 
     const getAllModels = async(): Promise<Models[]> =>{
         return (await fetch(`/api/models/getallmodels`)).json()
     }
 
-    const addModel = async (model: Models) => {
+    const addModel = async (model: Omit<Models, "id">, userId: string, file: File) => {
+        const dirName = file.name.split('.').reverse().pop()
+        const { data, error } = await supabase.storage.from('ModelsBucket').upload(`${userId}/${dirName}/${file.name}`, file)
+        if (error) {
+            console.error('An error has occured no model was uploaded: ', error)
+            throw error
+        }
+        const newModel = {...model, fileUrl: data?.path}
+        console.log(data?.path)
         return (await fetch(`/api/models/addmodel`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify(model)
+            body: JSON.stringify(newModel)
         }))
     }
 
@@ -31,6 +59,7 @@ export const useModels = ()=>{
     return {
         getModelById,
         getAllModels,
-        addModel
+        addModel,
+        getModelFileById
     }
 }
