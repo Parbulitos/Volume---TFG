637d0dfb3e25bc0ad0bd42bbbe93a14ed7b355e9
Parbulitos <cechavarne@gmail.com>
2024-05-12 19:13:46 +0200

    Foto de perfil

--- hooks/useAuth.tsx
+++ hooks/useAuth.tsx
@@ -1,20 +1,23 @@
 import { supabaseClient } from '@/database/utils';
 import { useUsers } from '@/hooks/useUsers';
+import { useImageKit } from '@/hooks/useImageKit';
 
 export const useAuth = () => {
     const signUpNewUser = async (
         email: string,
         password: string,
         name: string,
-        username: string
+        username: string,
+        avatarImage: File
     ) => {
         const { data, error } = await supabaseClient.auth.signUp({
             email: email,
             password: password,
         });
+        const avatarUrl = await useImageKit().uploadAvatar(avatarImage);
         if (!error && data.user != null && data.user.email != null) {
             await useUsers().addUser({
-                avatarUrl: null,
+                avatarUrl: avatarUrl,
                 name: name,
                 role: null,
                 email: data.user.email,
