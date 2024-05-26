e2cba5d0391b7f332654f35d393ac3afb9e084a5
Hugo Calzado <hcalzado@gmail.com>
2024-05-26 11:40:28 +0200

    añadida vista de la impresora

--- pages/print-area/index.tsx
+++ pages/print-area/index.tsx
@@ -3,12 +3,7 @@ import React from 'react';
 const Tutorials = () => {
     return (
         <div className="flex h-screen flex-col items-center justify-center gap-y-10">
-            <p>Work In Progress</p>
-            <div className="flex flex-row gap-2">
-                <div className="h-4 w-4 animate-bounce rounded-full bg-primary [animation-delay:.7s]"></div>
-                <div className="h-4 w-4 animate-bounce rounded-full bg-primary [animation-delay:.3s]"></div>
-                <div className="h-4 w-4 animate-bounce rounded-full bg-primary [animation-delay:.7s]"></div>
-            </div>
+            <iframe src="192.168.1.40"></iframe>
         </div>
     );
 };
